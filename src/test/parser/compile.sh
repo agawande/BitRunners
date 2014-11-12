d905c39afb3556b7e96ddbfad5f98c050b820d88
Isma <ismaha91@gmail.com>
2014-11-12 13:30:33 -0600

    Folder fix.

--- src/test/parser/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ ../../engine/parser.cpp test_client.cpp -o test
-
-echo "Parser test built"
--- src/test/parser/parser_test.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __PARSER_TEST_H__
-#define __PARSER_TEST_H__
-
-//Testing
-char* readNextInt(char*, int*);
-char* readNextFloat(char*, float*);
-
-#endif
