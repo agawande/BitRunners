abcc64901786e7b09bfd5904133167bd2ef6bee1
Ashlesh Gawande <agawande@memphis.edu>
2014-12-07 23:07:42 -0600

    Linking results file

--- src/engine/compile.sh
+++ src/engine/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+gcc --version
 echo "Compiling";
+ 
 gcc ADTs/berth.c ADTs/taxiway.c ADTs/plane.c parser.c simlib/simlib.c sim_io.c simulation.c gpct.c -o gpct -lm
 gcc gpct_kill.c -o gpct_kill
